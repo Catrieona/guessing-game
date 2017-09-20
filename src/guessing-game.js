1003d0b4d926e67c50fed7fe8eb93f6c617690e7
Ekaterina McArenko <polumna-la@mail.ru>
2017-09-21 01:49:28 +0300

    Version1

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+
+    	this.memory=[]
+    	this.tmp=0
+    }
 
     setRange(min, max) {
+    	for (i=min; i<max; i++)
+    		{this.memore.push(i)}
 
     }
 
     guess() {
+    	this.tmp=Math.floor(Math.random()*this.memory.length)
+    	return this.memory[this.tmp]
 
     }
 
     lower() {
+    	this.memory.splice(this.tmp)
 
     }
 
     greater() {
+    	this.memore.splice(0, this.tmp)
 
     }
 }
